6fcfce4779f64a75aa0294c91c1ca8e5e1a07649
yoga1422 <12050113952@students.uin-suska.ac.id>
2022-05-10 23:19:45 +0700

    Initial commit

---
+++ src/components/Accordion.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="accordion" id="accordionExample">
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="headingOne">
+        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Web Lukman Qur'an
+        </button>
+      </h2>
+      <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+        <div class="accordion-body">
+          <strong>Ini adalah Web Al-Qur'an Lukman Ahmadi.</strong> Disini menggunakan data dari API quran.api-docs.io
+        </div>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="headingTwo">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Isi Surat Al-Qari'ah , Al-Humazah , Al-Maun
+        </button>
+      </h2>
+      <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+        <div class="accordion-body">
+          <strong>Disini saya menggunakan 3 surat yaitu Surat Al-Qari'ah , Al-Humazah , Al-Maun.</strong>Diantaranya menggunakan api simpel , tajwid,menggunakan voice.
+        </div>
+      </div>
+    </div>
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="headingThree">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Sumber API
+        </button>
+      </h2>
+      <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+        <div class="accordion-body">
+          <strong>Sumber API yang digunakan dari API quran.api-docs.io</strong>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
